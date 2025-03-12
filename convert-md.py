5fe031e433c9fa2fd0cfcd007ce7e223e3642433
john-cashman <johncashmanireland@gmail.com>
2025-03-12 15:22:41 +0000

    Update convert-md.py

--- convert-md.py
+++ convert-md.py
@@ -142,7 +142,7 @@ def create_markdown_zip(df):
 
 # Streamlit app
 def main():
-    st.title("CSV to Grouped Markdown File Generator with SUMMARY.md")
+    st.title("Zendesk CSV to Markdown")
 
     # Displaying instructions
     st.info("""
